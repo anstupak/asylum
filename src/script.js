b4bf9afbc2df3f21d9f656ff95a16e36f9a29829
Anastasia Stupak <nastyamyaots@gmail.com>
2019-10-25 16:04:08 +0300

    optimize a page's layout

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Wait for loading of the page
 document.addEventListener('DOMContentLoaded', run);
 
 let header;
@@ -14,15 +15,24 @@ function run() {
     menuBtnText = document.getElementById('menu-button-text');
 
     const menuBtn = document.getElementById('menu-button');
+    // Function "toggleMenu" will be called for every click on the menu button.
     menuBtn.addEventListener('click', toggleMenu);
 }
 
+/**
+ * Handles clicks on the menu button: opens or hides the menu.
+ */
 function toggleMenu() {
+    // Hide the header's elements when the menu is opened
     header.classList.toggle('Header--with-menu');
+    // Hide the footer when the menu is opened
     footer.classList.toggle('Footer--with-menu');
+    // Shifts the page's content to the right.
     page.classList.toggle('Page--with-menu');
+    // Toggles the menu.
     menu.classList.toggle('Menu--closed');
 
+    // Change the menu button's text.
     const closed = menu.classList.contains('Menu--closed');
     menuBtnText.textContent = closed ? 'Menu' : 'Close'
 }
