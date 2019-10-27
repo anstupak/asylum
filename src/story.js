ff643a35ded2ebb0cd26a1c99d324f3d87dd1952
Anastasia Stupak <nastyamyaots@gmail.com>
2019-10-27 16:58:18 +0300

    add patient modal dialog

--- src/story.js
+++ src/story.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', run);
 
 function run() {
     initModal('story-hospital-modal');
+    initModal('story-patient-modal');
+
+
+    initSlider(
+        'StoryPatientSlider__card',
+        'StoryPatientSlider__card--active',
+        'StoryPatientSlider__card--closed',
+        'slider-arrow-left',
+        'slider-arrow-right'
+    );
 }
 
 function initModal(modalName) {
@@ -14,5 +24,47 @@ function initModal(modalName) {
         // Hide the header's elements when the menu is opened
         modal.toggle();
     });
+}
+
+function initSlider(elementClass, activeElementClass, closedClassName, leftBtnId, rightBtnId) {
+    const leftBtn = document.getElementById(leftBtnId)
+    const rightBtn = document.getElementById(rightBtnId)
+
+    let elements = nodeListToArr(document.querySelectorAll('.' + elementClass));
+
+    rightBtn.addEventListener('click', function() {
+        const current = elements[0];
+        elements = elements.slice(1);
+        elements.push(current);
+
+        for (let i = 0; i < elements.length; i++) {
+            const el = elements[i];
+            el.classList.remove('StoryPatientSlider__card--1')
+            el.classList.remove('StoryPatientSlider__card--2')
+            el.classList.remove('StoryPatientSlider__card--3')
+            el.classList.remove('StoryPatientSlider__card--4')
+
+            if (i !== elements.length - 1) {
+                el.classList.add('StoryPatientSlider__card--' + (i + 1))
+            } else {
+                el.classList.add('StoryPatientSlider__card--closed');
+                function closingHandler() {
+                    el.removeEventListener("transitionend", closingHandler);
+                    el.classList.remove('StoryPatientSlider__card--closed');
+                    el.classList.add('StoryPatientSlider__card--4')
+                }
+                el.addEventListener("transitionend", closingHandler, false);
+            }
+        }
+    });
+
+    function nodeListToArr(list) {
+        const arr = [];
+        for (let i = 0; i < list.length; i++) {
+            const el = list[i];
+            arr.push(el);
+        }
+        return arr
+    }
 }
 
